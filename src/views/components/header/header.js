d99f52608c1b61047326b4a9f6fcf9d3eba8bf7d
Arshansh <arshanshagarwal1@gmail.com>
2021-08-10 20:53:13 +0530

    finalize

--- src/views/components/header/header.js
+++ src/views/components/header/header.js
@@ -1,21 +1,13 @@
-import React, { useState } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 import Button from "../button";
-import Icon from "../icon";
 
 import "./header.css";
-
-const openHelp = () => {
-  console.log("hfkshdkfjks");
-};
 
 const Header = ({ authenticated, signOut }) => (
   <header className="header">
     <div className="g-row head-title-container">
       <h1 className="header__title">To-Do List</h1>
-      {/* <button onClick={openHelp} className="help-button">
-        <Icon name="help_outline" className="help-icon" />
-      </button> */}
     </div>
     <div className="g-row btn-container">
       {authenticated ? <Button onClick={signOut}>Sign out</Button> : null}
